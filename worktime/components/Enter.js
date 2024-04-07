d4b68bd7082b1a6eb702fd0f3645eb828c326772
and96437345 <and96437345@yandex.ru>
2024-04-07 11:28:39 +0300

    Registration, Enter

--- worktime/components/Enter.js
+++ worktime/components/Enter.js
@@ -1,47 +1,28 @@
 import React from 'react';
-import { View, Text, CheckBox, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
 const RegistrationPage = () => {
   return (
-    <View style={styles.createAccount}>
-        <Text style={styles.title}>СОЗДАНИЕ УЧЕТНОЙ ЗАПИСИ</Text>
-        <Text style={styles.account}>У Вас уже есть аккаунт?</Text>
+    <View style={styles.enter}>
+        <Text style={styles.title}>ВХОД</Text>
+        <Text style={styles.account}>У Вас еще нет аккаунта?</Text>
         <TouchableOpacity style={styles.accountButton}>
-              <Text style={styles.accountButtonText}>ВОЙТИ</Text>
+              <Text style={styles.accountButtonText}>ЗАРЕГИСТРИРОВАТЬСЯ</Text>
         </TouchableOpacity>
         <View style={styles.inputContainer}>
-          <TextInput style={styles.input} placeholder="Имя" />
-          <TextInput style={styles.input} placeholder="Фамилия" />
-          <TextInput style={styles.input} placeholder="Электронная почта" />
           <TextInput style={styles.input} placeholder="Логин" />
-          <TextInput style={styles.input} placeholder="Пароль" secureTextEntry={true} />         
-          <View style={styles.checkboxRow}>
-            <View style={styles.checkboxContainer}>
-              {/* <CheckBox
-                value={isSelected}
-                onValueChange={setSelection}
-                style={styles.checkbox}
-              /> */}
-              <Text style={styles.label}>Сотрудник</Text>
-            </View>
-            <View style={styles.checkboxContainer}>
-              {/* <CheckBox
-                value={isSelected}
-                onValueChange={setSelection}
-                style={styles.checkbox}
-              /> */}
-              <Text style={styles.label}>Руководитель</Text>
-            </View>
-          </View>
+          <TextInput style={styles.input} placeholder="Пароль" secureTextEntry={true} />
+          <TouchableOpacity style={styles.accountButton}>
+            <Text style={styles.accountPassword}>Забыл пароль?</Text>
+          </TouchableOpacity>
           <TouchableOpacity style={styles.accountCreateButton}>
-            <Text style={styles.accountCreateButtonText}>Создать аккаунт</Text>
+            <Text style={styles.accountCreateButtonText}>ВОЙТИ</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.accountButton}>
-            <Text style={styles.accountSMS}>Создать аккаунт по СМС</Text>
-          </TouchableOpacity>
-          
+            <Text style={styles.accountSMS}>Войти по СМС</Text>
+          </TouchableOpacity>        
         </View>
-      </View>
+    </View>
   );
 };
 
