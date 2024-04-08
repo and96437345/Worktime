fcfaeda735142c009cc36370141e4b1041c4d42e
Andrey <136622037+and96437345@users.noreply.github.com>
2024-04-08 21:37:54 +0300

    Registration, Enter, Navigator

--- worktime/App.js
+++ worktime/App.js
@@ -1,121 +1,46 @@
-import React, {useState} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import {CheckBox, View, Text, Image, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
-import BannerPage from './components/Banner';
-import RegistrationPage from './components/RegistrationPage';
-import EnterPage from './components/EnterPage';
-import {styles} from './styles/style';
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import {
+  CheckBox,
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import BannerPage from "./components/Banner";
+import RegistrationPage from "./components/RegistrationPage";
+import EnterPage from "./components/EnterPage";
+import { styles } from "./styles/style";
 // import CheckBox from '@react-native-community/checkbox';
 
 export default function App() {
   const [isSelected, setSelection] = useState(false);
-  
+
   const Stack = createNativeStackNavigator();
   const MyStack = () => {
     return (
       <NavigationContainer>
         <Stack.Navigator>
+          <Stack.Screen name="Registration" component={RegistrationPage} />
           <Stack.Screen
             name="Banner"
             component={BannerPage}
-            options={{title: 'Welcome'}}
+            options={{ title: "Welcome" }}
           />
-          <Stack.Screen
-          name="Registration" 
-          component={RegistrationPage}
-          />
-          <Stack.Screen
-          name="Enter" 
-          component={EnterPage}
-          />
+          <Stack.Screen name="Enter" component={EnterPage} />
         </Stack.Navigator>
       </NavigationContainer>
     );
   };
 
-  
   return (
     <ScrollView>
-      <View style={styles.container}> 
-        <View style={styles.header}>
-          {/* Лого */}
-          <Text style={styles.logo}>Logo</Text>
-          {/* Кнопки */}
-          <View style={styles.buttonsContainer}>
-          
-            <TouchableOpacity style={styles.button}>
-            <Image style={styles.image} source={require('./find.png')}
-              />
-              {/* <Text>Поиск</Text> */}           
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-            <Image style={styles.image} source={require('./people.png')}
-              />
-              {/* <Text>Сотрудники</Text> */}
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-            <Image style={styles.image} source={require('./cog.png')}
-              />
-              {/* <Text>Настройки</Text> */}
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.burger}>
-              <Text style={styles.burgerText}>☰</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        
-          <MyStack />
-        
-        <View style={styles.footer}>
-          <View style={styles.footerMenu}>
-              {/* Меню */}
-          <Text style={styles.logo}>Logo</Text>
-          {/* Кнопки */}
-          <View style={styles.buttonsContainer}>
-          
-            <TouchableOpacity style={styles.button}>
-            <Image style={styles.image} source={require('./find.png')}
-              />
-              {/* <Text>Поиск</Text> */}           
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-            <Image style={styles.image} source={require('./people.png')}
-              />
-              {/* <Text>Сотрудники</Text> */}
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-            <Image style={styles.image} source={require('./cog.png')}
-              />
-              {/* <Text>Настройки</Text> */}
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.burger}>
-              <Text style={styles.burgerText}>☰</Text>
-            </TouchableOpacity>
-          </View>
-          </View>    
-            <View style={styles.footerSupport}>
-              <Text style={styles.footerSupportTitle}>Поддержка:</Text>
-              <View>
-                <View style={styles.footerSupportList}>
-                  <Text style={styles.footerSupportItem}>Помощь по программе</Text>           
-                  <TouchableOpacity style={styles.burger}>
-                    <Text style={styles.footerSupportItem}>+7(777)777-77-77</Text>
-                  </TouchableOpacity>               
-                </View>
-                <View  style={styles.footerSupportList}>
-                  <Text style={styles.footerSupportItem}>FAQ</Text>
-                  <TouchableOpacity style={styles.burger}>
-                    <Text style={styles.footerSupportItem}>xakaton.2024@gmail.ru</Text>
-                  </TouchableOpacity>               
-                </View>
-              </View>
-            </View>
-        </View>
-      
-      </View>
+      <MyStack />
     </ScrollView>
-    
   );
 }
--- worktime/components/RegistrationPage.js
+++ worktime/components/RegistrationPage.js
@@ -1,48 +1,129 @@
-import React from 'react';
-import { View, Text, CheckBox, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import {styles} from '../styles/style';
+import React from "react";
+import {
+  Image,
+  View,
+  Text,
+  CheckBox,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { styles } from "../styles/style";
 
 const RegistrationPage = () => {
   return (
-    <View style={styles.createAccount}>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        {/* Лого */}
+        <Text style={styles.logo}>Logo</Text>
+        {/* Кнопки */}
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Image style={styles.image} source={require("../find.png")} />
+            {/* <Text>Поиск</Text> */}
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Image style={styles.image} source={require("../people.png")} />
+            {/* <Text>Сотрудники</Text> */}
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Image style={styles.image} source={require("../cog.png")} />
+            {/* <Text>Настройки</Text> */}
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.burger}>
+            <Text style={styles.burgerText}>☰</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.createAccount}>
         <Text style={styles.title}>СОЗДАНИЕ УЧЕТНОЙ ЗАПИСИ</Text>
         <Text style={styles.account}>У Вас уже есть аккаунт?</Text>
+
         <TouchableOpacity style={styles.accountButton}>
-              <Text style={styles.accountButtonText}>ВОЙТИ</Text>
+          <Text style={styles.accountButtonText}>ВОЙТИ</Text>
         </TouchableOpacity>
+
         <View style={styles.inputContainer}>
           <TextInput style={styles.input} placeholder="Имя" />
           <TextInput style={styles.input} placeholder="Фамилия" />
           <TextInput style={styles.input} placeholder="Электронная почта" />
           <TextInput style={styles.input} placeholder="Логин" />
-          <TextInput style={styles.input} placeholder="Пароль" secureTextEntry={true} />         
-          <View style={styles.checkboxRow}>
-            <View style={styles.checkboxContainer}>
-              {/* <CheckBox
+          <TextInput
+            style={styles.input}
+            placeholder="Пароль"
+            secureTextEntry={true}
+          />
+          {/* <View style={styles.checkboxRow}> */}
+          {/* <View style={styles.checkboxContainer}> */}
+          {/* <CheckBox
                 value={isSelected}
                 onValueChange={setSelection}
                 style={styles.checkbox}
               /> */}
-              <Text style={styles.label}>Сотрудник</Text>
-            </View>
-            <View style={styles.checkboxContainer}>
-              {/* <CheckBox
+          {/* <Text style={styles.label}>Сотрудник</Text> */}
+          {/* </View> */}
+          {/* <View style={styles.checkboxContainer}> */}
+          {/* <CheckBox
                 value={isSelected}
                 onValueChange={setSelection}
                 style={styles.checkbox}
               /> */}
-              <Text style={styles.label}>Руководитель</Text>
-            </View>
-          </View>
+          {/* <Text style={styles.label}>Руководитель</Text> */}
+          {/* </View> */}
+          {/* </View> */}
           <TouchableOpacity style={styles.accountCreateButton}>
             <Text style={styles.accountCreateButtonText}>Создать аккаунт</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.accountButton}>
             <Text style={styles.accountSMS}>Создать аккаунт по СМС</Text>
           </TouchableOpacity>
-          
         </View>
       </View>
+
+      <View style={styles.footer}>
+        <View style={styles.footerMenu}>
+          {/* Меню */}
+          <Text style={styles.logo}>Logo</Text>
+          {/* Кнопки */}
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity style={styles.button}>
+              <Image style={styles.image} source={require("../find.png")} />
+              {/* <Text>Поиск</Text> */}
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Image style={styles.image} source={require("../people.png")} />
+              {/* <Text>Сотрудники</Text> */}
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Image style={styles.image} source={require("../cog.png")} />
+              {/* <Text>Настройки</Text> */}
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.burger}>
+              <Text style={styles.burgerText}>☰</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.footerSupport}>
+          <Text style={styles.footerSupportTitle}>Поддержка:</Text>
+          <View>
+            <View style={styles.footerSupportList}>
+              <Text style={styles.footerSupportItem}>Помощь по программе</Text>
+              <TouchableOpacity style={styles.burger}>
+                <Text style={styles.footerSupportItem}>+7(777)777-77-77</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.footerSupportList}>
+              <Text style={styles.footerSupportItem}>FAQ</Text>
+              <TouchableOpacity style={styles.burger}>
+                <Text style={styles.footerSupportItem}>
+                  xakaton.2024@gmail.ru
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
   );
 };
 
